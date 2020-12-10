c36aa614a40300808123307c14bfaa55e65c68d9
GuoPuWen <1537990340@qq.com>
2020-12-10 15:29:19 +0800

    update autogithub.bat

--- autogithub.bat
+++ autogithub.bat
@@ -1,15 +1,15 @@
-@echo off
+﻿@echo off
 
 echo.
 echo                                      GitHub自动上传脚本
 echo ===================================================================================
 echo.
 
-set /p change=请输入commit名：
+
 
 
 git add .
-git commit -m %change%
+git commit 
 git push origin master
 
 echo.
